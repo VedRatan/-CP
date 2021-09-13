3e2dc27d2137decd33c630b36d25baef325078bd
VedRatan <vedratan8@gmail.com>
2021-09-13 12:38:39 +0530

    new one

---
+++ chess_tournament.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(false);
+cin.tie(0);
+    int t,n;
+    cin>>t;
+    while(t--) {
+        cin>>n;
+        int a[n];
+        for(int i=0; i<n; i++)
+        cin>>a[i];
+        if(n==2&&a[0]!=a[1])
+        cout<<"NO"<<endl;
+        else
+        {
+            char arr[n][n];
+            if(n%2==0)
+            {
+                for(int i=0;i<n;i++)
+                {
+                    for(int j=0;j<n;j++)
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
