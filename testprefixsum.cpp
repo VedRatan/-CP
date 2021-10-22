9092775f62dfdd619c052d60653233662d5d48df
VedRatan <vedratan8@gmail.com>
2021-10-22 23:40:59 +0530

    new one

---
+++ testprefixsum.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+const int N = 1e5+10;
+int a[N];
+int pf[N];
+ 
+int main() {
+  int n;
+  cin >> n;
+  for(int i=0; i<n; i++){
+    cin >> a[i];
+    pf[i] = pf[i-1] + a[i];
+  }
+  int q;
+  cin >> q;
+  while(q--){
+    int l, r;
+    cin >> l >> r;
+  //Calculating sum from r to l.
+    cout << pf[r] - pf[l-1] << endl;
+     
+  }
+   return 0;
+}
