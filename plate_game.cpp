b9e033bc18be20327d9302705d886dbe859c2c2f
VedRatan <vedratan8@gmail.com>
2021-11-18 23:37:13 +0530

    new one

---
+++ plate_game.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ios::sync_with_stdio(false);
+cin.tie(0);
+    int a,b,k;
+    cin>>a>>b>>k;
+    if((2*k)<=a&&(2*k)<=b)
+    cout<<"First"<<endl;
+    else
+    cout<<"Second"<<endl;
+    return 0;
+}
